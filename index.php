77f7f600ee19e26099987662635b3a7329fbc6ad
Jacob Erickson <jerickson@csumb.edu>
2018-04-02 18:26:04 +0000

    Initial adding of files to use

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+    //Put session handling here
+    
+    //Put database handling here
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> -------- Catalog </title>
+        <style type="text/css">
+            @import url("styles.css");
+        </style>
+    </head>
+    <body>
+        
+        <header>
+            
+        </header>
+        
+        <main>
+            
+            <div id="criteria">
+                <!-- Put the form and search criteria here -->
+            </div>
+            
+            <div id="results">
+                
+                <?php
+                
+                    //loop through to show the results here
+                
+                ?>
+                
+            </div>
+            
+        </main>
+        
+        <footer>
+            
+        </footer>
+
+    </body>
+</html>
